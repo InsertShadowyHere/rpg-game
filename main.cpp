9067f27e048ae049e4f9191f8c71d26a763035ba
shdwy <100291718+InsertShadowyHere@users.noreply.github.com>
2025-02-28 13:11:13 -0500

    Update main.cpp
    can't do it through clion because it's bugging out

--- main.cpp
+++ main.cpp
@@ -7,28 +7,23 @@
 
 using namespace sf;
 
-std::deque<Packet*> packet_queue;
+std::deque<Packet *> packet_queue;
 
 void queue_packets() {
     std::cout << "Packet thread is running!" << std::endl;
     UdpSocket socket;
     if (socket.bind(54000) != sf::Socket::Status::Done)
-    {
         std::cout << "Error" << std::endl;
-    }
     while (true) {
         Packet *packet = new Packet();
         std::optional<IpAddress> sender;
         unsigned short port;
-        if (socket.receive(*packet, sender, port) != Socket::Status::Done)
-        {
+        if (socket.receive(*packet, sender, port) != Socket::Status::Done) {
             // error...
         }
-        std::cout << "Received " << packet->getData() << " from " << sender->toString() << " on port " << port << std::endl;
+        std::cout << "DEBUG: Received data from " << sender->toString() << " on port " << port << std::endl;
         packet_queue.push_back(packet);
     }
-
-
 }
 
 using namespace std;
@@ -50,6 +45,7 @@ public:
         cout << "Player " << id << " data: " << data << endl;
         return 0;
     }
+
     long deblob() {
         data -= 1;
         return 0;
@@ -68,7 +64,7 @@ private:
 
 class Game {
 public:
-    vector<Player*> players;
+    vector<Player *> players;
 
     // Fetch player data, add player to vector
     void add_player(const string &uname) {
@@ -77,10 +73,9 @@ public:
     }
 
     void test_player_vector() {
-        for (const Player *i : players) {
-            std::cout << "Player " << i->id << ": " <<  players[0]->uname << std::endl;
+        for (const Player *i: players) {
+            std::cout << "Player " << i->id << ": " << players[0]->uname << std::endl;
         }
-
     }
 
     void process(Packet *packet) {
@@ -104,7 +99,7 @@ public:
                 // PosUpdate
                 int pid;
                 *packet >> pid;
-                for (Player *i : players)
+                for (Player *i: players)
                     if (i->id == pid) {
                         *packet >> i->x >> i->y;
                         cout << "Player " << i->id << " position updated." << endl;
@@ -113,28 +108,28 @@ public:
                 break;
             }
         }
-
     }
 };
 
 
 int main() {
-    const string x = "I'm a string!";
-    cout << x << std::endl;
-    Player player("Davis");
-    sf::Texture bg_texture("assets/bg.png");
-    sf::Sprite bg_sprite(bg_texture);
-    Game game = Game();
+    // texture initialization (eventually change to region struct)
+    // eventually should be handled through initialization/region-swap packets
+    Texture bg_texture("assets/bg.png");
+    Sprite bg_sprite(bg_texture);
+
+    // Main game object
+    Game game;
     game.add_player("Davis");
     game.test_player_vector();
 
     // create the window and initialize some things
-    sf::RenderWindow window(sf::VideoMode({800, 600}), "My window");
+    RenderWindow window(sf::VideoMode({800, 600}), "My window");
     window.setFramerateLimit(60); // call it once after creating the window
 
     // test image
-    sf::Texture texture("assets/jared.png");
-    sf::Sprite sprite(texture);
+    Texture texture("assets/jared.png");
+    Sprite sprite(texture);
     sprite.setPosition({0, 380});
 
 
@@ -147,50 +142,44 @@ int main() {
      * Render entities */
     std::thread receiver(queue_packets);
     receiver.detach();
+    // eventually should be triggered by initialization packets
     while (window.isOpen()) {
-
+        // Processes packets
         if (!packet_queue.empty()) {
-            cout << "Not empty!" << endl;
-            cout << packet_queue.size() << endl;
             game.process(packet_queue.front());
             packet_queue.pop_front();
         }
+
         // Check all window events triggered this run-through of the loop
         while (const std::optional event = window.pollEvent()) {
             // "close requested" event: we close the window
             if (event->is<sf::Event::Closed>())
                 window.close();
-
-            // Handle key presses only when in focus
-            if (window.hasFocus()) {
-                // If mouse button is pressed:
-                // move Jared
-                // Set window position
-                if (event->is<sf::Event::MouseButtonPressed>()) {
-                }
-            }
         }
         if (isKeyPressed(sf::Keyboard::Scan::Right)) {
             cout << "hello" << endl;
         }
 
-        // if the left button is down, give bg a lil push
-        if (isButtonPressed(sf::Mouse::Button::Left)) {
-            // Character moves left, screen moves right
-            if (sf::Mouse::getPosition(window).x < window.getSize().x / 2) {
-                sprite.move({5, -5});
-                bg_sprite.move({-5, 0});
+        // Handle key presses only when in focus
+        if (window.hasFocus()) {
+            // if the left button is down, give bg a lil push
+            if (isButtonPressed(sf::Mouse::Button::Left)) {
+                // Character moves left, screen moves right
+                if (sf::Mouse::getPosition(window).x < window.getSize().x / 2) {
+                    sprite.move({5, -5});
+                    bg_sprite.move({-5, 0});
+                }
+                // character moves right, screen moves left
+                else
+                    bg_sprite.move({5, 0});
             }
-            // character moves right, screen moves left
-            else
-                bg_sprite.move({5, 0});
         }
-
 
         // clear the window with black color
         window.clear(sf::Color::Black);
 
-        // draw everything here...
+
+        // DRAW EVERYTHING HERE
         // window.draw(...);
         window.draw(sprite);
         window.draw(bg_sprite);
